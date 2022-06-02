629c6698976ee62138fd9a6643f40088e0f3f67c
Nestor Victorino <contactonestorvictorino@gmail.com>
2022-06-01 23:47:45 -0400

    .

--- components/pages/upDashboard.tsx
+++ components/pages/upDashboard.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const UpDashboard = () => {
   return (
     <div className='py-3 px-5 d-flex flex-column justify-content-center align-items-center text-white' style={{ border: '1px solid white', marginTop:'8rem' }}>
-      <span className='fw-normal' style={{fontSize:'2.3rem'}}>Nestor Victorino</span>
-      <span className='fw-light' style={{fontSize:'1.2rem'}}>Front End Developer</span>
+      <span className='fw-normal h1'>Nestor Victorino</span>
+      <span className='fw-light h3'>Front End Developer</span>
     </div>
   )
 }
